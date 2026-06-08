6f375b9dbac16136d320c78523d42f2703d547b5
nayanaa10 <nayanaa.chatterjee1@gmail.com>
2026-06-08 16:01:06 +0530

    ok

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Import named exports — you must use the EXACT same names
 // and wrap them in curly braces { }
-import { PI, add, multiply } from './math.js';
+import{ add, multiply } from './math.js';
  
 // Now we can use PI, add, and multiply freely
 
-console.log(PI);
+
 // → 3.14159
 console.log(add(2, 3));
 // → 5
@@ -13,8 +13,7 @@ console.log(multiply(4, 5));
  
 // Import a default export — no curly braces needed
 // You can give it ANY name you like
-import greet from './greet.js';
-console.log(greet('Bob'));
+
 // → Hello, Bob!
  
 // Import EVERYTHING from a file under one namespace object
